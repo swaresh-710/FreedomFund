f14fb7c833e110460faa6b6a03c4ba4bb740ed8f
swaresh binaykiya <72277918+swaresh-710@users.noreply.github.com>
2023-02-19 19:57:22 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork : 'goerli',
+    networks : {
+      hardhat : {},
+      goerli : {
+        url : 'https://rpc.ankr.com/eth_goerli',
+        account: ['0X${process.env.PRIVATE_KEY}']
+      }
+    
+
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
